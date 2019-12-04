e4264f891767e68abdca1197d78b105e36320641
Roman Misnikov <r.misnikov@cft.ru>
2019-12-04 14:12:42 +0300

    Сделал метод загрузки публичным

--- Sources/ApiKit/ApiKit.swift
+++ Sources/ApiKit/ApiKit.swift
@@ -4,7 +4,7 @@ import PromiseKit
 public class ApiKit {
 	public init() {}
 
-	func fetchData(from url: URL) -> Promise<Data> {
+	public func fetchData(from url: URL) -> Promise<Data> {
 		return Promise<Data> { sink in
 			URLSession.shared.dataTask(with: url) { data, _, error in
 				sink.resolve(data, error)
